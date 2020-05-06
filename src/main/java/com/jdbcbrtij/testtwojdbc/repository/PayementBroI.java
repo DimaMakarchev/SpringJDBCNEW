263f7bb966fdab1f1380ccfc636e6bcf391515b5
Dima Makarchev <d.makarchev2411@gmail.com>
2020-05-06 10:29:17 +0300

    Test br_2

--- src/main/java/com/jdbcbrtij/testtwojdbc/repository/PayementBroI.java
+++ src/main/java/com/jdbcbrtij/testtwojdbc/repository/PayementBroI.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface PayementBroI {
     void creatBD();
 
-    void save(BankBro bankBro);
+    void save (BankBro bankBro);
 
     int update(BankBro bankBro);
 
